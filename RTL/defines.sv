40929d3aa9465a4bd38f51b7d74be0381d22467d
mb2997 <mbhavsar2997@gmail.com>
2025-01-30 13:47:59 -0800

    Delete RTL directory

--- RTL/defines.sv
+++
@@ -1,6 +0,0 @@
-// `define WCLK_FRQ 80						//Unit: MHz
-// `define RCLK_FRQ 50						//Unit: MHz
-// `define WCLK_T/2 ((1/`WCLK_FRQ)/2)
-// `define RCLK_T/2 ((1/`RCLK_FRQ)/2)
-// `define ADDR_WIDTH 8
-// `define DATA_WIDTH 32
