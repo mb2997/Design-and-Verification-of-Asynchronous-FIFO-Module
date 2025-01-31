88720c92cc8b2b3e7cea1c5f762140c1b4ada390
mb2997 <mbhavsar2997@gmail.com>
2025-01-30 16:07:34 -0800

    Add files via upload
    Updated TB

--- TB/async_fifo_top_tb.sv
+++ TB/async_fifo_top_tb.sv
@@ -7,7 +7,7 @@ module async_fifo_top_tb;
 	parameter real WCLK_T = ((1/WCLK_FRQ)*1000);
 	parameter real RCLK_T = ((1/RCLK_FRQ)*1000);
 	parameter DATA_WIDTH=8;
-	parameter ADDR_WIDTH=10;
+	parameter ADDR_WIDTH=4;
 
 	logic [DATA_WIDTH-1:0] rdata;
 	logic wfull,rempty;
@@ -17,7 +17,7 @@ module async_fifo_top_tb;
 	
 	int error = 0;
 
-	asynch_fifo_dut #(8,10) DUT(.*);
+	asynch_fifo_dut #(8,4) DUT(.*);
 	
 	`ifdef DEBUG
 		initial 
@@ -48,9 +48,6 @@ module async_fifo_top_tb;
 		else begin
 			wdata = i;
 			winc = 1;
-			@(posedge wclk);
-			@(posedge wclk);
-			winc = 0;
 		end
 	endtask
 	
@@ -62,33 +59,37 @@ module async_fifo_top_tb;
 		end
 		else begin
 			rinc = 1;
-			@(posedge rclk);
-			@(posedge rclk);
-			rinc = 0;
 		end
 	endtask
 	
 	initial begin
 		wrst_n = 0;
 		rrst_n = 0;
-		#20;
+		fork
+			@(negedge wclk);
+			@(negedge rclk);
+		join
 		wrst_n = 1;
 		rrst_n = 1;
-		#20;
 		
+		@(negedge wclk);
 		//write to FIFO
-		for (int i = 0; i < 2**ADDR_WIDTH; i++) begin
+		for (int i = 0; i < 2**ADDR_WIDTH; i++)
+		begin
 			WriteFIFO(i);
-			#100;
+			@(negedge wclk);
+			winc = 0;
 		end
 		
-		//check if FIFO is full
-		WriteFIFO(500);
-		#100;
+		@(negedge rclk);
+		// //check if FIFO is full
+		// WriteFIFO(500);
+		// #100;
 
 		for (int j = 0; j < 2**ADDR_WIDTH; j++) begin
 			ReadFIFO();
-			#100;
+			@(negedge rclk);
+			rinc = 0;
 			if (rdata != j) begin
 				$display("ERROR");
 				$display("%t Expected: %0d. Actual: %0d",$time, j, rdata);
@@ -96,9 +97,9 @@ module async_fifo_top_tb;
 			end
 		end
 		
-		//check if fifo is empty
-		ReadFIFO();
-		#100;
+		// //check if fifo is empty
+		// ReadFIFO();
+		// #100;
 
 		if (error==2) 
 			$display("FIFO passed all tests!");
