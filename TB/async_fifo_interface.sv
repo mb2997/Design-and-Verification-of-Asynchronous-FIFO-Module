443568c9c91a7e34b501cd854ae3aeddc939e689
Anvitha Mohan Shirva <anvithas@mo.ece.pdx.edu>
2025-02-15 11:36:26 -0800

    interface and kgm

---
+++ TB/async_fifo_interface.sv
@@ -0,0 +1,12 @@
+interface async_fifo_interface #(
+	parameter DATA_WIDTH=8, 
+	parameter ADDR_WIDTH=4)(
+	input logic wclk, wrst_n,rclk, rrst_n);
+	
+	logic [DATA_WIDTH-1:0] rdata;
+	logic wfull;
+	logic rempty;
+	logic [DATA_WIDTH-1:0] wdata;
+	logic winc;
+	logic rinc;
+endinterface
