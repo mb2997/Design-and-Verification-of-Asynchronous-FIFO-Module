716057c1efdca3eef77acbdf0f0d6356ce1eb980
anvithashirva <130870681+anvithashirva@users.noreply.github.com>
2025-02-24 11:35:27 -0800

    fifo pkg

--- TEST/async_fifo_pkg.sv
+++ TEST/async_fifo_pkg.sv
@@ -1,18 +1,22 @@
 `include "async_fifo_interface.sv"
-
+import uvm_pkg::*;
 package async_fifo_pkg;
+
+	`include "uvm_macros.svh"
 
 	int no_of_trans = 16;
 	bit sb_enable = 1;
 	
-	`include "transaction.sv"
-	`include "generator.sv"
+	`include "seq_item.sv"
+	`include "gen_seq_item.sv"
+	`include "sequencer.sv"
 	`include "driver.sv"
 	`include "monitor.sv"
 	`include "scoreboard.sv"
+	`include "agent.sv"
 	`include "environment.sv"
 
-    //Testcases
+    //Testcases  ??
 	`include "async_fifo_write_read_test.sv"
 	`include "async_fifo_write_full_test.sv"
 	`include "async_fifo_read_empty_test.sv"
