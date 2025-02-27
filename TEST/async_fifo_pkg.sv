dcf85e906a7688d7b630765ea82d93ade95e688f
mb2997 <mbhavsar2997@gmail.com>
2025-02-26 17:40:21 -0800

    Add files via upload

--- TEST/async_fifo_pkg.sv
+++ TEST/async_fifo_pkg.sv
@@ -1,31 +1,29 @@
-`include "async_fifo_interface.sv"
-import uvm_pkg::*;
+`include "async_fifo_if.sv"
+
 package async_fifo_pkg;
+    
+    int no_of_trans = 20;
+    bit sb_enable = 1;
 
-	`include "uvm_macros.svh"
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
 
-	int no_of_trans = 16;
-	bit sb_enable = 1;
-	
-	`include "seq_item.sv"
-	`include "gen_seq_item.sv"
-	`include "sequencer.sv"
-	`include "driver.sv"
-	`include "monitor.sv"
-	`include "scoreboard.sv"
-	`include "agent.sv"
-	`include "environment.sv"
+    `include "async_fifo_trans.sv"
+    
+    `include "async_fifo_seqs.sv"
 
-    //Testcases  ??
-	`include "async_fifo_write_read_test.sv"
-	`include "async_fifo_write_full_test.sv"
-	`include "async_fifo_read_empty_test.sv"
-	`include "async_fifo_b2b_write_read_test.sv"
-	`include "async_fifo_read_at_empty_test.sv"
-	`include "async_fifo_write_at_full_test.sv"
-	`include "async_fifo_simultaneous_write_read_test.sv"
-	`include "async_fifo_reset_test.sv"
-	`include "async_fifo_write_read_max_data_range_test.sv"
-	`include "async_fifo_base_test.sv"
+    `include "async_fifo_seqr.sv"
+
+    `include "async_fifo_drv.sv"
+
+    `include "async_fifo_mon.sv"
+
+    `include "async_fifo_agent.sv"
+
+    `include "async_fifo_sb.sv"
+
+    `include "async_fifo_env.sv"
+
+    `include "async_fifo_test.sv"
 
 endpackage
